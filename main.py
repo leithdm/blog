d9a86cea93fe1083332b5459e1c94a709612e95e
leithdm <leithdm@gmail.com>
2021-03-09 15:26:21 +0000

    update env.py

--- main.py
+++ main.py
@@ -225,6 +225,6 @@ def delete_post(post_id):
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("5000"))
+        host=os.environ.get("IP","0.0.0.0"),
+        port=int(os.environ.get("PORT"))
     )
